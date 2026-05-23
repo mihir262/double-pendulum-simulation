4ddc1ed6b6682140e73dd838347b99b3a96d984e
mihir262 <mihirdeogaonkar@gmail.com>
2026-05-23 14:13:10 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <raylib.h>
+
+int main(){
+    InitWindow(WIDTH, HEIGHT, "Double Pendulum");
+
+    while(!WindowShouldClose){
+        BeginDrawing();
+
+        EndDrawing();
+    }
+    return 0;
+}
+
